9c94e3538500645553c1a9222d0e6cf918b12a71
msdos41 <tjm.mosquito@gmail.com>
2024-12-11 20:50:54 +0800

    chore: update next.config.ts for deployment on Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath,
+  assetPrefix,
+  output: "export",
 };
+
 
 export default nextConfig;
