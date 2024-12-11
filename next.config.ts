01b8f78ed81c70a80ee821a17b675ac7c400181a
msdos41 <tjm.mosquito@gmail.com>
2024-12-11 21:26:10 +0800

    fix: next.config.ts configuration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 // const nextConfig: NextConfig = {
 //   /* config options here */
 // };
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "";
-let basePath = "";
+let assetPrefix = '';
+let basePath = '';
 
 if (isGithubActions) {
   // 去掉 `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  const repo = process.env.GITHUB_REPOSITORY!.replace(/.*?\//, '');
 
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
@@ -19,8 +19,7 @@ if (isGithubActions) {
 const nextConfig: NextConfig = {
   basePath,
   assetPrefix,
-  output: "export",
+  output: 'export',
 };
-
 
 export default nextConfig;
